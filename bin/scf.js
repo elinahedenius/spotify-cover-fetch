2501532e737d0af5ac944f28ca360900abee013c
elinahedenius <bohedenius@protonmail.com>
2020-05-27 21:43:14 +0200

    added two options in the cli script too

--- bin/scf.js
+++ bin/scf.js
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
+const scf = require("../index.js")
 
-require('../index.js')(process.argv.slice(2))
+actualArg = process.argv.slice(2)[0]
+
+async function main(arg){
+  let toGet = process.argv.slice(3);
+
+  if(arg == "-download"){
+    scf.download(toGet);
+    return;
+  }
+  if (arg == "-getsrc"){
+    let src = await scf.getSrc(toGet);
+    console.log(`Image src: ${src}`);
+  }
+  else {
+    console.error("you need to use the argument '-download' or '-getsrc'");
+    return;
+  }
+}
+
+main(actualArg);
